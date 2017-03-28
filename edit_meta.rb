0000335db5acdd1980ca8a3193f90c8b39d2d1c5
Monique Rio <nique.rio@gmail.com>
2017-03-28 14:14:11 -0400

    added inns of court

--- edit_meta.rb
+++ edit_meta.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 require 'tempfile'
 require 'yaml'
 require 'csv'
-csv = CSV.foreach('./bransle.csv') do | row|
+csv = CSV.foreach('./inns.csv') do | row|
 #directories = Dir.glob('*').select {|f| File.directory? f and f != "metadata" and f !=  "test"}
 #directories.each do  |dir|
   editions = {'sheet_music' => []}
@@ -11,10 +11,10 @@ csv = CSV.foreach('./bransle.csv') do | row|
   #name = "#{arranger}'s Arrangement"
   source = 'http://pennsicdance.aands.org/pennsic45/PennsicPile45.pdf'
   mf_name = 'Pennsic Pile Edition'
-  filename = "#{row[1]}.pdf"
-  editions['sheet_music'].push({'slug' => row[2], 'name' => 'Fake Book Edition', 'music_files' => [ 'filename'=> filename, 'source' => source, 'name' => mf_name ]})
-  FileUtils.cp("/home/mrio/dev/pile/#{row[1]}.pdf", "./#{row[0]}/#{row[1]}.pdf")
-  sm_slugs.push(row[2])
+  filename = "#{row[2]}.pdf"
+  editions['sheet_music'].push({'slug' => row[1], 'name' => row[3], 'music_files' => [ 'filename'=> filename, 'source' => source, 'name' => mf_name ]})
+  FileUtils.cp("/home/mrio/dev/pile/#{filename}", "./#{row[0]}/#{filename}")
+  sm_slugs.push(row[1])
         
     
     
@@ -23,9 +23,9 @@ csv = CSV.foreach('./bransle.csv') do | row|
   t_file = Tempfile.new('filename_temp.txt')
   t_file2 = Tempfile.new('filename_temp2.txt')
   if hash['instructions'][0].key?('sheet_music')
-    hash['instructions'][0]['sheet_music'].push(row[2])
+    hash['instructions'][0]['sheet_music'].push(row[1])
   else
-    hash['instructions'][0]['sheet_music'] = [row[2]]
+    hash['instructions'][0]['sheet_music'] = [row[1]]
   end
   #hash['sources'] = ['slug' => row[1]] 
     
