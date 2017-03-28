716fef404ac5a45908fe3a2c61c8d70b6d7bcb33
Monique Rio <nique.rio@gmail.com>
2017-03-28 13:51:09 -0400

    added Bransles

--- edit_meta.rb
+++ edit_meta.rb
@@ -2,30 +2,31 @@ require 'fileutils'
 require 'tempfile'
 require 'yaml'
 require 'csv'
-csv = CSV.foreach('./period.csv') do | row|
+csv = CSV.foreach('./bransle.csv') do | row|
 #directories = Dir.glob('*').select {|f| File.directory? f and f != "metadata" and f !=  "test"}
 #directories.each do  |dir|
-  next if row[1].nil?
   editions = {'sheet_music' => []}
+  editions = YAML.load_file("./#{row[0]}/media.yaml") if File.exists?("./#{row[0]}/media.yaml")
   sm_slugs = []
-  Dir["/home/mrio/dev/pile/#{row[1]}/*.ly"].each do |ly|
-    basename = File.basename(ly)
-    #name = "#{arranger}'s Arrangement"
-    source = 'http://pennsicdance.aands.org/pennsic45/PennsicPile45.pdf'
-    mf_name = 'Pennsic Pile Edition'
-    filename = basename.gsub('ly','pdf')
-    editions['sheet_music'].push({'slug' => row[2], 'name' => row[3], 'music_files' => [ 'filename'=> filename, 'source' => source, 'name' => mf_name ]})
-    #FileUtils.cp("/home/mrio/dev/pile/#{filename}", "./#{row[0]}/#{filename}")
-    sm_slugs.push(row[2])
+  #name = "#{arranger}'s Arrangement"
+  source = 'http://pennsicdance.aands.org/pennsic45/PennsicPile45.pdf'
+  mf_name = 'Pennsic Pile Edition'
+  filename = "#{row[1]}.pdf"
+  editions['sheet_music'].push({'slug' => row[2], 'name' => 'Fake Book Edition', 'music_files' => [ 'filename'=> filename, 'source' => source, 'name' => mf_name ]})
+  FileUtils.cp("/home/mrio/dev/pile/#{row[1]}.pdf", "./#{row[0]}/#{row[1]}.pdf")
+  sm_slugs.push(row[2])
         
     
     
     #puts row[1] if editions['sheet_music'].count == 0
-  end
   hash = YAML.load_file("./#{row[0]}/metadata.yaml")
   t_file = Tempfile.new('filename_temp.txt')
   t_file2 = Tempfile.new('filename_temp2.txt')
-  hash['instructions'][0]['sheet_music'] = sm_slugs
+  if hash['instructions'][0].key?('sheet_music')
+    hash['instructions'][0]['sheet_music'].push(row[2])
+  else
+    hash['instructions'][0]['sheet_music'] = [row[2]]
+  end
   #hash['sources'] = ['slug' => row[1]] 
     
   t_file.puts editions.to_yaml
